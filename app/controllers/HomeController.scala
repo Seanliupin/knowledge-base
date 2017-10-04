1a88fa75b780fbc6fcdc1d7a375128ee90be0143
Sean <mingyi_127@126.com>
2017-10-04 11:15:18 +0800

    add db

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,48 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import play.api.db._
 import play.api.mvc._
+import play.api.libs.json.Json
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents, db: Database) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+
+    var result = "DB User:"
+    val conn = db.getConnection()
+    try {
+      val rs = conn.createStatement().executeQuery("SELECT USER()")
+      println("look " + rs)
+      while (rs.next()) {
+        result += rs.getString(1)
+      }
+    } finally {
+      conn.close()
+    }
+
+
+    Ok(views.html.index("re = < " + result + " >"))
+  }
+
+  def ticketsAvailable = Action { request =>
+    val availableTickets = 1000
+    val result = Json.obj(
+      "result" -> "ok",
+      "ticketQuantity" -> availableTickets)
+    Ok(result)
   }
 }
--- build.sbt
+++ build.sbt
@@ -8,8 +8,11 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.12.3"
 
 libraryDependencies += guice
-libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
+libraryDependencies += jdbc
+libraryDependencies += evolutions
 
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
+libraryDependencies += "com.h2database" % "h2" % "1.4.192"
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "com.dotgoing.controllers._"
 
