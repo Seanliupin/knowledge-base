ce21dac46e46217a37b19aedd5a2b727eff183d9
Sean <mingyi_127@126.com>
2017-10-04 17:33:42 +0800

    add query

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,11 +2,7 @@ package controllers
 
 import javax.inject._
 
-import com.dotgoing.prac.QueryString
-import play.api.data.Form
-import play.api.data.Forms._
 import play.api.db._
-import play.api.libs.json.Json
 import play.api.mvc._
 
 /**
@@ -16,40 +12,14 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, db: Database) extends AbstractController(cc) {
 
-
   def index() = Action { implicit request: Request[AnyContent] =>
-
-    //    var result = "DB User:"
-    //    val conn = db.getConnection()
-    //    try {
-    //      val rs = conn.createStatement().executeQuery("SELECT USER()")
-    //      println("look " + rs)
-    //      while (rs.next()) {
-    //        result += rs.getString(1)
-    //      }
-    //    } finally {
-    //      conn.close()
-    //    }
-
-
     Ok(views.html.index(""))
   }
 
-  def ticketsAvailable = Action { request: Request[AnyContent] =>
-    val availableTickets = 1000
-    val result = Json.obj(
-      "result" -> "ok",
-      "ticketQuantity" -> availableTickets)
-    Ok(result)
+  def search(query: String) = Action { request: Request[AnyContent] =>
+//    Ok("query = " + query)
+    Ok(views.html.index(query))
   }
 
-  def search = Action { request: Request[AnyContent] =>
-
-    Ok(views.html.index(""))
-  }
-
-  def test = Action { request: Request[AnyContent] =>
-
-    Ok(views.html.test(""))
-  }
+  def test = TODO
 }
