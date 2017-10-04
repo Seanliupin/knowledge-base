266e326c398d5f1e318b3f013eefb22738e96c08
Sean <mingyi_127@126.com>
2017-10-04 22:37:06 +0800

    refactor

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,7 +4,7 @@ import javax.inject._
 
 import play.api.db._
 import play.api.mvc._
-import resource._
+import service.NoteService
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -12,41 +12,13 @@ import resource._
   */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, db: Database) extends AbstractController(cc) {
-
-  val dir = "/Users/seanliu/Note/"
-
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index("", ""))
   }
 
   def search(query: String) = Action { request: Request[AnyContent] =>
-    val s = new StringBuilder
-    val fileName = dir + "daily/dailyLog-2017-9-10.md"
-
-    val tokens = query.trim.split("[\\s]+")
-
-    def renderHit(text: String, token: String): String = {
-      text.replaceAll(token, "<strong class=\"text-danger\">" + token + "</strong>")
-    }
-
-    for (source <- managed(scala.io.Source.fromFile(fileName))) {
-      for (line <- source.getLines) {
-        val contain = tokens.exists(token => {
-          line.contains(token)
-        })
-
-        if (contain) {
-          var renderedLine = line
-          tokens.foreach(token => {
-            renderedLine = renderHit(renderedLine, token)
-          })
-          s.append(renderedLine)
-          s.append("<br/> <br/>")
-        }
-      }
-    }
-
-    Ok(views.html.index(query, s.toString()))
+    val tokens = query.trim.split("[\\s]+").map(_.trim).filter(_.length > 0).toList
+    Ok(views.html.index(query, NoteService.search(tokens)))
   }
 
   def test = TODO
