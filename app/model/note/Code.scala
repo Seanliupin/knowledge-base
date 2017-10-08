23f9a0bb48039c0121bbd9f3a64f92673a163562
Sean <mingyi_127@126.com>
2017-10-08 15:19:00 +0800

    refactor

--- app/model/note/Code.scala
+++
@@ -1,7 +0,0 @@
-package model.note
-
-/**
-  * Author: Sean
-  * Date: 5/10/2017
-  * Time: 10:20 AM
-  */
--- app/model/note/Note.scala
+++ app/model/note/Note.scala
@@ -20,73 +20,74 @@ case class Note(fileName: String) extends KnowledgeBase {
     var codeBlock = Code(None, None)
     var commentBlock = Comment(None, None)
 
-    for (source <- managed(scala.io.Source.fromFile(fileName))) {
-      for (line <- source.getLines) {
-        line match {
-          case Extractor.codeFooterExtractor() => {
-            if (codeBlock.isValid) {
-              piece.addLine(codeBlock)
-              codeBlock = Code(None, None)
-            } else {
-              codeBlock = Code(Some(""), Some(""))
-            }
+    for {
+      source <- managed(scala.io.Source.fromFile(fileName))
+      line <- source.getLines
+    } {
+      line match {
+        case Extractor.codeFooterExtractor() => {
+          if (codeBlock.isValid) {
+            piece.addLine(codeBlock)
+            codeBlock = Code(None, None)
+          } else {
+            codeBlock = Code(Some(""), Some(""))
           }
-          case Extractor.commentFooterExtractor() => {
-            if (commentBlock.isValid) {
-              piece.addLine(commentBlock)
-              commentBlock = Comment(None, None)
-            } else {
-              commentBlock = Comment(Some(""), Some(""))
-            }
-          }
-          case Extractor.codeHeaderExtractor(lan, title) => {
-            if (!codeBlock.isEmpty) {
-              piece.addLine(codeBlock)
-            }
-            codeBlock = Code(Some(lan), Some(title.trim))
-          }
-          case Extractor.commentHeaderExtractor(ctype, title) => {
-            if (!commentBlock.isEmpty) {
-              piece.addLine(commentBlock)
-            }
-
-            commentBlock = Comment(Some(ctype), Some(title))
-          }
-          case code if codeBlock.isValid => codeBlock.addLine(code)
-          case comment if commentBlock.isValid => commentBlock.addLine(comment)
-
-          case Extractor.titleExtractor(title) => {
-            if (piece.isValid) {
-              pieces = pieces ++ List(piece)
-            }
-            piece = Piece(Some(title), Option(fileName))
-          }
-          case Extractor.tagsExtractor(tags) if piece.isValid =>
-            tags.split(StringUtil.whiteSpaceSegmenter)
-              .toList.map(_.trim)
-              .filter(_.length > 0)
-              .foreach(keyWord => piece.addLine(KeyWord(keyWord)))
-          case Extractor.keysExtractor(keys) if piece.isValid =>
-            keys.split(StringUtil.whiteSpaceSegmenter)
-              .toList.map(_.trim)
-              .filter(_.length > 0)
-              .foreach(keyWord => piece.addLine(KeyWord(keyWord)))
-          case Extractor.timeExtractor(time) if piece.isValid => piece.setTime(time)
-          case Extractor.WebExtractor(title, url, comment) if piece.isValid => piece.addLine(Web(title, url, comment))
-          case Extractor.WebItemExtractor(title, url, comment) if piece.isValid => piece.addLine(Web(title, url, comment))
-          case Extractor.bookExtractor(title, url, comment) if piece.isValid => piece.addLine(Book(title, url, comment))
-          case Extractor.timeExtractor(time) if piece.isValid => piece.setTime(time)
-          case Extractor.subTitleExtractor(subTitle) if piece.isValid => piece.addLine(SubTitle(subTitle))
-          case Extractor.typedTipExtractor(tipType, tip) if piece.isValid => piece.addLine(Tip(tip, Some(tipType)))
-          case Extractor.typeLessTipExtractor(tip) if piece.isValid => piece.addLine(Tip(tip, None))
-          case _ if piece.isValid => piece.addLine(Line(line))
-          case _ =>
         }
+        case Extractor.commentFooterExtractor() => {
+          if (commentBlock.isValid) {
+            piece.addLine(commentBlock)
+            commentBlock = Comment(None, None)
+          } else {
+            commentBlock = Comment(Some(""), Some(""))
+          }
+        }
+        case Extractor.codeHeaderExtractor(lan, title) => {
+          if (!codeBlock.isEmpty) {
+            piece.addLine(codeBlock)
+          }
+          codeBlock = Code(Some(lan), Some(title.trim))
+        }
+        case Extractor.commentHeaderExtractor(ctype, title) => {
+          if (!commentBlock.isEmpty) {
+            piece.addLine(commentBlock)
+          }
+
+          commentBlock = Comment(Some(ctype), Some(title))
+        }
+        case code if codeBlock.isValid => codeBlock.addLine(code)
+        case comment if commentBlock.isValid => commentBlock.addLine(comment)
+
+        case Extractor.titleExtractor(title) => {
+          if (piece.isValid) {
+            pieces = piece +: pieces
+          }
+          piece = Piece(Some(title), Option(fileName))
+        }
+        case Extractor.tagsExtractor(tags) if piece.isValid =>
+          tags.split(StringUtil.whiteSpaceSegmenter)
+            .toList.map(_.trim)
+            .filter(_.length > 0)
+            .foreach(keyWord => piece.addLine(KeyWord(keyWord)))
+        case Extractor.keysExtractor(keys) if piece.isValid =>
+          keys.split(StringUtil.whiteSpaceSegmenter)
+            .toList.map(_.trim)
+            .filter(_.length > 0)
+            .foreach(keyWord => piece.addLine(KeyWord(keyWord)))
+        case Extractor.timeExtractor(time) if piece.isValid => piece.setTime(time)
+        case Extractor.WebExtractor(title, url, comment) if piece.isValid => piece.addLine(Web(title, url, comment))
+        case Extractor.WebItemExtractor(title, url, comment) if piece.isValid => piece.addLine(Web(title, url, comment))
+        case Extractor.bookExtractor(title, url, comment) if piece.isValid => piece.addLine(Book(title, url, comment))
+        case Extractor.timeExtractor(time) if piece.isValid => piece.setTime(time)
+        case Extractor.subTitleExtractor(subTitle) if piece.isValid => piece.addLine(SubTitle(subTitle))
+        case Extractor.typedTipExtractor(tipType, tip) if piece.isValid => piece.addLine(Tip(tip, Some(tipType)))
+        case Extractor.typeLessTipExtractor(tip) if piece.isValid => piece.addLine(Tip(tip, None))
+        case _ if piece.isValid => piece.addLine(Line(line))
+        case _ =>
       }
     }
 
     if (piece.isValid) {
-      pieces = pieces ++ List(piece)
+      pieces = piece +: pieces
     }
 
     pieces
