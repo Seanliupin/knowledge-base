2753e40d825b41f3fb4d055d6286fa5f4ca81fe1
Sean <mingyi_127@126.com>
2019-03-24 15:49:34 +0800

    refactor

--- app/model/note/NoteFile.scala
+++ app/model/note/NoteFile.scala
@@ -10,25 +10,12 @@ import resource.managed
   */
 case class NoteFile(fileName: String) {
 
-  /**
-    * parse piece of information
-    **/
-  def notes: List[Note] = {
+  def linesToNotes(lines: List[String], fN: String, globalTitle: Option[String], globalYear: Option[String]): List[Note] = {
     var pieces: List[Note] = List()
     var note = Note(None, None)
     var codeBlock = Code(None, None)
-    var commentBlock = Memo(None, None)
+    var memoBlock = Memo(None, None)
     var globalTags: List[KeyWord] = List()
-    var globalTitle: Option[String] = None
-    var globalYear: Option[String] = None
-
-    fileName match {
-      case Extractor.titleYearExtractor(_, title, year, _) => {
-        globalTitle = Some(title)
-        globalYear = Some(year)
-      }
-      case _ =>
-    }
 
     def getHashCode(str: String): String = {
       s"${str.hashCode}"
@@ -46,7 +33,7 @@ case class NoteFile(fileName: String) {
         } {
           oldTitle.line match {
             case Extractor.dayMonthExtractor(month, day) => {
-              fixNote = Note(Some(Title(titleFromFile, Some(getHashCode(s"$fileName$oldTitle")))), note.fileName)
+              fixNote = Note(Some(Title(titleFromFile, Some(getHashCode(s"$fN$oldTitle")))), note.fileName)
               fixNote.setTime(Time(s"$year/$month/$day"))
               fixNote.setLines(note.getLines)
             }
@@ -59,86 +46,105 @@ case class NoteFile(fileName: String) {
       pieces
     }
 
-    for {
-      source <- managed(scala.io.Source.fromFile(fileName, "UTF-8"))
-      line <- source.getLines
-    } {
-      line match {
-        case Extractor.globalTagsExtractor(tags) if !note.isValid => {
-          globalTags = tags.split(StringUtil.whiteSpaceSegmenter)
-            .toList.map(KeyWord(_))
-        }
-
-        case Extractor.codeFooterExtractor() => {
-          if (codeBlock.isValid) {
-            note.addLine(codeBlock)
-            codeBlock = Code(None, None)
-          } else {
-            codeBlock = Code(Some(""), Some(""))
-          }
-        }
-        case Extractor.memoFooterExtractor() => {
-          if (commentBlock.isValid) {
-            note.addLine(commentBlock)
-            commentBlock = Memo(None, None)
-          } else {
-            commentBlock = Memo(Some(""), Some(""))
-          }
-        }
-        case Extractor.codeHeaderExtractor(lan, title) => {
-          if (!codeBlock.isEmpty) {
-            note.addLine(codeBlock)
-          }
-          codeBlock = Code(Some(lan), Some(title.trim))
-        }
-        case Extractor.memoHeaderExtractor(ctype, title) => {
-          if (!commentBlock.isEmpty) {
-            note.addLine(commentBlock)
-          }
-
-          commentBlock = Memo(Some(ctype), Some(title))
-        }
-        case code if codeBlock.isValid => codeBlock.addLine(code)
-        case comment if commentBlock.isValid => commentBlock.addLine(comment)
-
-        case Extractor.titleExtractor(title) => {
-          if (note.isValid) {
-            addNote(note)
-          }
-          note = Note(Some(Title(title, Some(getHashCode(s"$fileName$title")))), Option(fileName))
-        }
-        case Extractor.tagsExtractor(tags) if note.isValid =>
-          tags.split(StringUtil.whiteSpaceSegmenter)
-            .map(KeyWord)
-            .foreach(note.addLine)
-        case Extractor.keysExtractor(keys) if note.isValid =>
-          keys.split(StringUtil.whiteSpaceSegmenter)
-            .map(KeyWord)
-            .foreach(note.addLine)
-        case Extractor.timeExtractor(time) if note.isValid => note.setTime(Time(time))
-        case Extractor.idExtractor(id) if note.isValid => note.addLine(Id(id.trim))
-        case Extractor.WebExtractor(title, url, comment) if note.isValid => note.addLine(Web(title, url, comment))
-        case Extractor.WebItemExtractor(title, url, comment) if note.isValid => note.addLine(Web(title, url, comment))
-        case Extractor.bookExtractor(title, url, comment) if note.isValid => note.addLine(Book(title, url, comment))
-        case Extractor.subTitleExtractor(subTitle) if note.isValid => note.addLine(SubTitle(subTitle))
-        case Extractor.scriptExtractor(src, des) if note.isValid => note.addLine(Script(src, des))
-        case Extractor.typedTipExtractor(tipType, tip) if note.isValid => {
-          if (tipType.trim.length == 0) {
-            note.addLine(Tip(tip, None))
-          } else {
-            note.addLine(Tip(tip, Some(tipType)))
-          }
-        }
-        case Extractor.typeLessTipExtractor(tip) if note.isValid => note.addLine(Tip(tip, None))
-        case _ if note.isValid => note.addLine(Line(line))
-        case _ =>
+    lines.foreach {
+      case Extractor.globalTagsExtractor(tags) if !note.isValid => {
+        globalTags = tags.split(StringUtil.whiteSpaceSegmenter)
+          .toList.map(KeyWord(_))
       }
+
+      case Extractor.codeFooterExtractor() => {
+        if (codeBlock.isValid) {
+          note.addLine(codeBlock)
+          codeBlock = Code(None, None)
+        } else {
+          codeBlock = Code(Some(""), Some(""))
+        }
+      }
+      case Extractor.memoFooterExtractor() => {
+        if (memoBlock.isValid) {
+          note.addLine(memoBlock)
+          memoBlock = Memo(None, None)
+        } else {
+          memoBlock = Memo(Some(""), Some(""))
+        }
+      }
+      case Extractor.codeHeaderExtractor(lan, title) => {
+        if (!codeBlock.isEmpty) {
+          note.addLine(codeBlock)
+        }
+        codeBlock = Code(Some(lan), Some(title.trim))
+      }
+      case Extractor.memoHeaderExtractor(ctype, title) => {
+        if (!memoBlock.isEmpty) {
+          note.addLine(memoBlock)
+        }
+
+        memoBlock = Memo(Some(ctype), Some(title))
+      }
+      case code if codeBlock.isValid => codeBlock.addLine(code)
+      case comment if memoBlock.isValid => memoBlock.addLine(comment)
+
+      case Extractor.titleExtractor(title) => {
+        if (note.isValid) {
+          addNote(note)
+        }
+        note = Note(Some(Title(title, Some(getHashCode(s"$fileName$title")))), Option(fileName))
+      }
+      case Extractor.tagsExtractor(tags) if note.isValid =>
+        tags.split(StringUtil.whiteSpaceSegmenter)
+          .map(KeyWord)
+          .foreach(note.addLine)
+      case Extractor.keysExtractor(keys) if note.isValid =>
+        keys.split(StringUtil.whiteSpaceSegmenter)
+          .map(KeyWord)
+          .foreach(note.addLine)
+      case Extractor.timeExtractor(time) if note.isValid => note.setTime(Time(time))
+      case Extractor.idExtractor(id) if note.isValid => note.addLine(Id(id.trim))
+      case Extractor.WebExtractor(title, url, comment) if note.isValid => note.addLine(Web(title, url, comment))
+      case Extractor.WebItemExtractor(title, url, comment) if note.isValid => note.addLine(Web(title, url, comment))
+      case Extractor.bookExtractor(title, url, comment) if note.isValid => note.addLine(Book(title, url, comment))
+      case Extractor.subTitleExtractor(subTitle) if note.isValid => note.addLine(SubTitle(subTitle))
+      case Extractor.scriptExtractor(src, des) if note.isValid => note.addLine(Script(src, des))
+      case Extractor.typedTipExtractor(tipType, tip) if note.isValid => {
+        if (tipType.trim.length == 0) {
+          note.addLine(Tip(tip, None))
+        } else {
+          note.addLine(Tip(tip, Some(tipType)))
+        }
+      }
+      case Extractor.typeLessTipExtractor(tip) if note.isValid => note.addLine(Tip(tip, None))
+      case line if note.isValid => note.addLine(Line(line))
+      case _ =>
     }
 
     if (note.isValid) {
       addNote(note)
     }
-
     pieces
+  }
+
+  /**
+    * parse piece of information
+    **/
+  def notes: List[Note] = {
+    if (fileName.trim.isEmpty) {
+      return List()
+    }
+
+    var globalTitle: Option[String] = None
+    var globalYear: Option[String] = None
+
+    fileName match {
+      case Extractor.titleYearExtractor(_, title, year, _) => {
+        globalTitle = Some(title)
+        globalYear = Some(year)
+      }
+      case _ =>
+    }
+
+    for {source <- managed(scala.io.Source.fromFile(fileName, "UTF-8"))} {
+      return linesToNotes(source.getLines().toList, fileName, globalTitle, globalYear)
+    }
+    List()
   }
 }
